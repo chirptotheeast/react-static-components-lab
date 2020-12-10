6a925f2c08b68d6de5a5adf1d614c875bb66ff72
chirptotheeast <alyssa.easterly@yahoo.com>
2020-12-10 15:30:19 -0500

    Done.

--- src/GraceHopperQuoteComponent.js
+++ src/GraceHopperQuoteComponent.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
-export class GraceHopperQuoteComponent extends Component {
+class GraceHopperQuoteComponent extends Component {
+
+
+	
 	render() {
 		return (
 			<div className="bar" id="hopper">
@@ -15,3 +18,6 @@ export class GraceHopperQuoteComponent extends Component {
 		);
 	}
 }
+
+
+export default GraceHopperQuoteComponent;
